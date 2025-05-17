ae750329f70b3ee63d3fbc65c054bacdc5718d96
Include caleb.fun <oasis.mystre@gmail.com>
2025-05-17 11:40:21 +0100

    feat: bug fix

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,42 @@ import { db } from "./instances";
 import { format } from "./utils/format";
 import { checkSubscriptions } from "./jobs/check-subscriptions";
 
-export const main = (bot: Telegraf) => {
-  async function main(server: FastifyInstance, bot: Telegraf) {
-    registerBot(bot);
+async function main(server: FastifyInstance, bot: Telegraf) {
+  registerBot(bot);
 
-    const promises = [];
+  const promises = [];
 
-    bot.catch((error) => console.error(error));
+  bot.catch((error) => console.error(error));
+  if (process.env.RENDER_EXTERNAL_HOSTNAME) {
+    server.post(
+      format("/telegraf/%", bot.secretPathComponent()),
+      (await bot.createWebhook({
+        domain: process.env.RENDER_EXTERNAL_HOSTNAME,
+      })) as any
+    );
+  } else
     promises.push(
       bot.launch().then(() => console.log("bot running in background"))
     );
 
-    promises.push(
-      server.listen({
-        host: process.env.HOST ? process.env.HOST : "0.0.0.0",
-        port: process.env.PORT ? Number(process.env.PORT!) : 10004,
-      }),
-      cron.schedule("0 */1 * * *", () => {
-        checkSubscriptions(db, bot).catch((error) => {
-          console.error(error);
-        });
-      })
-    );
+  promises.push(
+    server.listen({
+      host: process.env.HOST ? process.env.HOST : "0.0.0.0",
+      port: process.env.PORT ? Number(process.env.PORT!) : 10004,
+    }),
+    cron.schedule("0 */1 * * *", () => {
+      checkSubscriptions(db, bot).catch((error) => {
+        console.error(error);
+      });
+    })
+  );
 
-    process.once("SIGINT", () => bot.stop("SIGINT"));
-    process.once("SIGTERM", () => bot.stop("SIGTERM"));
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-    return Promise.all(promises);
-  }
+  return Promise.all(promises);
+}
 
-  const server = fastify({ logger: true, ignoreTrailingSlash: true });
-
-  main(server, bot);
-};
-
+const server = fastify({ logger: true, ignoreTrailingSlash: true });
 const bot = new Telegraf(getEnv("TELEGRAM_ACCESS_TOKEN"));
-main(bot);
+main(server, bot);
