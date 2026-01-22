7c8e569de4cfe82a30374b1fb358e86b5d9fcd9a
oasisMystre <oasis.mystre@gmail.com>
2026-01-22 23:16:49 +0100

    feat: www

--- www/app/root.tsx
+++ www/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { FacebookPixel } from "./components/FacebookPixel";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <FacebookPixel pixelId="1095996719243423" />
         <ScrollRestoration />
         <Scripts />
       </body>
